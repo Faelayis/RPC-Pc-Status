9c0d50a817b173a27016e52e61162a379aee988e
Faelayis <48393914+Faelayis@users.noreply.github.com>
2021-08-31 01:59:32 +0700

    0.1.9-beta
    improve check updates

--- src/updata.js
+++ src/updata.js
@@ -1,46 +1,77 @@
+const { dialog } = require('electron')
+const { autoUpdater } = require("electron-updater");
+const path = require('path');
+const iconpath = path.join(__dirname, 'assets/icon/connected.png');
+let allow = true;
+
 exports.Checkupdates = () => {
-    const { dialog } = require('electron')
-    const { autoUpdater } = require("electron-updater");
-    const path = require('path');
-    autoUpdater.currentChannel = autoUpdater.channel;
-    autoUpdater.allowPrerelease = true;
-    autoUpdater.allowDowngrade = true;
-    autoUpdater.setFeedURL({
-        provider: "github",
-        owner: "Faelayis",
-        repo: "RPC-Pc-Status",
-    });
-    autoUpdater.checkForUpdates();
-    autoUpdater.on('checking-for-update', () => {
-        console.log('Checking for update...');
-    })
-    autoUpdater.on('update-available', (info) => {
-        console.log('Update available.');
-    })
-    autoUpdater.on('update-not-available', (info) => {
-        console.log('Update not available.');
-    })
-    autoUpdater.on('error', message => {
-        console.error('There was a problem updating the application')
-        console.error(message)
-    })
-    autoUpdater.on('download-progress', (progressObj) => {
-        let log_message = "Download speed: " + progressObj.bytesPerSecond;
-        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-        console.log(log_message);
-    })
-    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-        const dialogOpts = {
-            type: 'none',
-            buttons: ['Restart', 'Later'],
-            title: `RPC Pc Status Update`,
-            message: process.platform === 'win32' ? releaseNotes : releaseName,
-            detail: `${event.releaseName} \nA new version has been downloaded. Restart the application to apply the updates.`,
-            icon: path.join(__dirname, 'assets/icon/connected.png'),
-        }
-        dialog.showMessageBox(dialogOpts).then((returnValue) => {
-            if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    if (allow === true) {
+        allow = false;
+        autoUpdater.currentChannel = autoUpdater.channel;
+        autoUpdater.allowPrerelease = true;
+        autoUpdater.allowDowngrade = true;
+        autoUpdater.setFeedURL({
+            provider: "github",
+            owner: "Faelayis",
+            repo: "RPC-Pc-Status",
+        });
+        autoUpdater.checkForUpdates();
+        autoUpdater.once('checking-for-update', () => {
+            console.log('Checking for update...');
         })
-    })
+        autoUpdater.once('update-available', (info) => {
+            console.log('Update available.');
+        })
+        autoUpdater.once('update-not-available', (info, event) => {
+            dialog.showMessageBox({
+                type: 'info',
+                buttons: ['ok'],
+                title: 'RPC Pc Status Update',
+                message: 'Update not available :)',
+                noLink: true,
+                icon: iconpath,
+            }).then((returnValue) => {
+                if (returnValue.response === 0) {
+                    allow = true;
+                };
+            })
+        })
+        autoUpdater.once('error', message => {
+            dialog.showMessageBox({
+                type: 'error',
+                buttons: ['ok'],
+                title: 'RPC Pc Status Update Error',
+                message: 'There was a problem updating the application',
+                detail: `${message}`,
+                noLink: true,
+            }).then((returnValue) => {
+                if (returnValue.response === 0) {
+                    allow = true;
+                };
+            })
+        })
+        autoUpdater.once('download-progress', (progressObj) => {
+            let log_message = "Download speed: " + progressObj.bytesPerSecond;
+            log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+            log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+            console.log(log_message);
+        })
+        autoUpdater.once('update-downloaded', (event, releaseNotes, releaseName) => {
+            dialog.showMessageBox({
+                type: 'info',
+                buttons: ['Restart App', 'Later'],
+                title: 'RPC Pc Status Update',
+                message: process.platform === 'win32' ? releaseNotes : releaseName,
+                detail: `${event.releaseName} \nA new version has been downloaded. Restart the application to apply the updates.`,
+                noLink: true,
+                icon: path.join(__dirname, 'assets/icon/connected.png'),
+            }).then((returnValue) => {
+                if (returnValue.response === 0) {
+                    allow = true, autoUpdater.quitAndInstall();
+                } else if (returnValue.response === 1) {
+                    allow = true;
+                };
+            })
+        })
+    }
 };
