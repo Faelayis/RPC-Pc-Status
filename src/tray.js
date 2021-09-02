581b0eecd4a15fe853404ff213a59518c06c8689
Faelayis <48393914+Faelayis@users.noreply.github.com>
2021-09-02 14:30:17 +0700

    Update tray.js
    fix icon not real time

--- src/tray.js
+++ src/tray.js
@@ -1,118 +1,127 @@
+/* eslint-disable no-undef */
 const { app, Menu, Tray, nativeImage } = require("electron");
 const path = require("path");
 const Presence = require("./RichPresence");
 const { Checkupdates } = require("./updata");
 const { seticon } = require("./store");
-
+// const { CreateWindow } = require("./BrowserWindow");
 let tray = null;
 
 exports.tray = () => {
+  const iconPath = "assets/icon/notconnected.png";
   if (tray) {
-    tray.destroy();
+    tray.setImage(nativeImage.createFromPath(path.join(__dirname, iconPath)));
+  } else {
+    tray = new Tray(nativeImage.createFromPath(path.join(__dirname, iconPath)));
   }
-  app.whenReady().then(() => {
+  tray.setTitle("Pc Status");
+  tray.setToolTip("Pc Status");
+  tray.setIgnoreDoubleClickEvents(true);
+  // tray.on("click", function () {
+  //   CreateWindow();
+  // });
+};
+
+exports.trayupdata = () => {
+  if (tray) {
     const iconPath =
       Presence.user === undefined
         ? "assets/icon/notconnected.png"
         : "assets/icon/connected.png";
-    // eslint-disable-next-line no-undef
-    tray = new Tray(nativeImage.createFromPath(path.join(__dirname, iconPath)));
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: `Status : ${Presence.user ? "Connected" : "Not connected"}`,
-        type: "normal",
-        enabled: false,
-      },
-      {
-        label: `User : ${Presence.user ? Presence.user : "Not found"}`,
-        type: "normal",
-        enabled: false,
-      },
-      {
-        label: `Version : ${app.getVersion()}`,
-        type: "normal",
-        enabled: false,
-      },
-      { type: "separator" },
-      {
-        label: "Theme",
-        submenu: [
-          {
-            label: "large Image",
-            submenu: [
-              {
-                label: "Dark",
-                type: "normal",
-                click: () => {
-                  seticon("icon_dark");
-                },
+    tray.setImage(nativeImage.createFromPath(path.join(__dirname, iconPath)));
+  }
+  tray.setContextMenu(Menu.buildFromTemplate([
+    {
+      label: `Status : ${Presence.user ? "Connected" : "Not connected"}`,
+      type: "normal",
+      enabled: false,
+    },
+    {
+      label: `User : ${Presence.user ? Presence.user : "Not found"}`,
+      type: "normal",
+      enabled: false,
+    },
+    {
+      label: `Version : ${app.getVersion()}`,
+      type: "normal",
+      enabled: false,
+    },
+    { type: "separator" },
+    {
+      label: "Theme",
+      submenu: [
+        {
+          label: "large Image",
+          submenu: [
+            {
+              label: "Dark",
+              type: "normal",
+              click: () => {
+                seticon("icon_dark");
               },
-              {
-                label: "White",
-                type: "normal",
-                click: () => {
-                  seticon("icon_white");
-                },
+            },
+            {
+              label: "White",
+              type: "normal",
+              click: () => {
+                seticon("icon_white");
               },
-              {
-                label: "Red",
-                type: "normal",
-                click: () => {
-                  seticon("icon_red");
-                },
+            },
+            {
+              label: "Red",
+              type: "normal",
+              click: () => {
+                seticon("icon_red");
               },
-              {
-                label: "Yellow",
-                type: "normal",
-                click: () => {
-                  seticon("icon_yellow");
-                },
+            },
+            {
+              label: "Yellow",
+              type: "normal",
+              click: () => {
+                seticon("icon_yellow");
               },
-              {
-                label: "Lime",
-                type: "normal",
-                click: () => {
-                  seticon("icon_lime");
-                },
+            },
+            {
+              label: "Lime",
+              type: "normal",
+              click: () => {
+                seticon("icon_lime");
               },
-              {
-                label: "Aqua",
-                type: "normal",
-                click: () => {
-                  seticon("icon_aqua");
-                },
+            },
+            {
+              label: "Aqua",
+              type: "normal",
+              click: () => {
+                seticon("icon_aqua");
               },
-              {
-                label: "Blue",
-                type: "normal",
-                click: () => {
-                  seticon("icon_blue");
-                },
+            },
+            {
+              label: "Blue",
+              type: "normal",
+              click: () => {
+                seticon("icon_blue");
               },
-              {
-                label: "Orange",
-                type: "normal",
-                click: () => {
-                  seticon("icon_orange");
-                },
+            },
+            {
+              label: "Orange",
+              type: "normal",
+              click: () => {
+                seticon("icon_orange");
               },
-            ],
-          },
-        ],
-      },
-      { type: "separator" },
-      {
-        label: "Check for updates..",
-        type: "normal",
-        click: () => {
-          Checkupdates();
+            },
+          ],
         },
+      ],
+    },
+    { type: "separator" },
+    {
+      label: "Check for updates..",
+      type: "normal",
+      click: () => {
+        Checkupdates();
       },
-      { type: "separator" },
-      { label: "Quit Pc Status", type: "normal", click: () => app.quit() },
-    ]);
-    tray.setTitle("Pc Status");
-    tray.setToolTip("Pc Status");
-    tray.setContextMenu(contextMenu);
-  });
-};
+    },
+    { type: "separator" },
+    { label: "Quit Pc Status", type: "normal", click: () => app.quit() },
+  ]));
+}
