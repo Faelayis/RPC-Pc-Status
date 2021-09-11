6d75d8d899720e16ea9dc8c4fe0743a5d96b7302
Faelayis <Faelayis@users.noreply.github.com>
2021-09-11 12:08:00 +0000

    Prettified Code!

--- src/page/script.js
+++ src/page/script.js
@@ -1,4 +1,3 @@
-
 const { ipcRenderer } = require("electron");
 const store = require("../store");
 // const log = require("electron-log");
@@ -14,13 +13,14 @@ document.getElementById("button-url-2").value = store.buttonsCustom[3];
 
 // eslint-disable-next-line no-unused-vars
 function getInputValue() {
-    // let inputVal = [];
-    // inputVal[0] = document.getElementById("details").value;
-    // inputVal[1] = document.getElementById("state").value;
-    let inputValbutton = [
-        document.getElementById("button-label-1").value,
-        document.getElementById("button-url-1").value,
-        document.getElementById("button-label-2").value,
-        document.getElementById("button-url-2").value];
-    ipcRenderer.send('asynchronous-buttonsinput', inputValbutton);
+  // let inputVal = [];
+  // inputVal[0] = document.getElementById("details").value;
+  // inputVal[1] = document.getElementById("state").value;
+  let inputValbutton = [
+    document.getElementById("button-label-1").value,
+    document.getElementById("button-url-1").value,
+    document.getElementById("button-label-2").value,
+    document.getElementById("button-url-2").value,
+  ];
+  ipcRenderer.send("asynchronous-buttonsinput", inputValbutton);
 }
