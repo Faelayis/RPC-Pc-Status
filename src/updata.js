058d4eaa1e2a6094fe0eb434ba99510cb5bcb3b4
Faelayis <48393914+Faelayis@users.noreply.github.com>
2021-09-12 19:16:46 +0700

    Releases 2.0.1

--- src/updata.js
+++ src/updata.js
@@ -2,5 +2,5 @@ const updateApp = require("update-electron-app");
 updateApp({
   updateInterval: "10 minutes",
   logger: require("electron-log"),
-  notifyUser: false,
+  autoquitandInstall: true,
 });
