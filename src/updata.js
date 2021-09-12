8fc5abcc9f1c7fe7cfea3f892c194a7ba60673a0
Faelayis <Faelayis@users.noreply.github.com>
2021-09-12 22:27:44 +0000

    Prettified Code!

--- src/updata.js
+++ src/updata.js
@@ -1,10 +1,16 @@
 const process = require("process");
-const path = require('path')
-const os = require('os')
-const { format } = require('util')
-const package = require('../package.json')
+const path = require("path");
+const os = require("os");
+const { format } = require("util");
+const package = require("../package.json");
 const log = require("electron-log");
-const { app, autoUpdater, dialog, Notification, nativeImage } = require("electron");
+const {
+  app,
+  autoUpdater,
+  dialog,
+  Notification,
+  nativeImage,
+} = require("electron");
 const iconpath = nativeImage.createFromPath(
   // eslint-disable-next-line no-undef
   path.join(__dirname, "icon/updateavailable.png")
@@ -13,23 +19,31 @@ autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = "info";
 
 const userAgent = format(
-  '%s/%s (% s: %s)',
+  "%s/%s (% s: %s)",
   package.name,
   package.version,
   os.platform(),
   os.arch()
-)
+);
 
-const supportedPlatforms = ['darwin', 'win32']
-const feedURL = `https://update.electronjs.org/${package.author.name}/RPC-Pc-Status/${process.platform}-${process.arch}/${app.getVersion()}`
-const requestHeaders = { 'User-Agent': userAgent }
+const supportedPlatforms = ["darwin", "win32"];
+const feedURL = `https://update.electronjs.org/${
+  package.author.name
+}/RPC-Pc-Status/${process.platform}-${process.arch}/${app.getVersion()}`;
+const requestHeaders = { "User-Agent": userAgent };
 let allow = true;
 
 exports.Checkupdates = (silent) => {
   if (allow === true) {
     allow = false;
-    if (typeof process !== 'undefined' && process.platform && !supportedPlatforms.includes(process.platform)) {
-      log.warn(`Electron's autoUpdater does not support the '${process.platform}' platform`)
+    if (
+      typeof process !== "undefined" &&
+      process.platform &&
+      !supportedPlatforms.includes(process.platform)
+    ) {
+      log.warn(
+        `Electron's autoUpdater does not support the '${process.platform}' platform`
+      );
       if (silent) {
         dialog
           .showMessageBox({
@@ -47,9 +61,9 @@ exports.Checkupdates = (silent) => {
           });
       }
     }
-    log.info('feedURL', feedURL)
-    log.info('requestHeaders', requestHeaders)
-    autoUpdater.setFeedURL(feedURL, requestHeaders)
+    log.info("feedURL", feedURL);
+    log.info("requestHeaders", requestHeaders);
+    autoUpdater.setFeedURL(feedURL, requestHeaders);
     autoUpdater.checkForUpdates();
     autoUpdater.once("checking-for-update", () => {
       log.info(`Checking for update...`);
@@ -105,32 +119,41 @@ exports.Checkupdates = (silent) => {
         ")";
       log.info(log_message);
     });
-    autoUpdater.once('update-downloaded', (event, releaseNotes, releaseName, releaseDate, updateURL) => {
-      log.warn('update-downloaded', [event, releaseNotes, releaseName, releaseDate, updateURL])
-      allow = false;
-      if (silent) {
-        dialog
-          .showMessageBox({
-            type: "info",
-            buttons: ["Restart App", "Later"],
-            title: "RPC Pc Status Update",
-            detail: `A new version has been downloaded. Restart the application to apply the updates.`,
-            noLink: true,
-            icon: iconpath,
-          })
-          .then((returnValue) => {
-            if (returnValue.response === 0) {
-              allow = true;
-              autoUpdater.quitAndInstall();
-              app.exit(0);
-            } else if (returnValue.response === 1) {
-              allow = true;
-            }
-          });
-      } else if (silent === false) {
-        autoUpdater.quitAndInstall();
-        app.exit(0)
+    autoUpdater.once(
+      "update-downloaded",
+      (event, releaseNotes, releaseName, releaseDate, updateURL) => {
+        log.warn("update-downloaded", [
+          event,
+          releaseNotes,
+          releaseName,
+          releaseDate,
+          updateURL,
+        ]);
+        allow = false;
+        if (silent) {
+          dialog
+            .showMessageBox({
+              type: "info",
+              buttons: ["Restart App", "Later"],
+              title: "RPC Pc Status Update",
+              detail: `A new version has been downloaded. Restart the application to apply the updates.`,
+              noLink: true,
+              icon: iconpath,
+            })
+            .then((returnValue) => {
+              if (returnValue.response === 0) {
+                allow = true;
+                autoUpdater.quitAndInstall();
+                app.exit(0);
+              } else if (returnValue.response === 1) {
+                allow = true;
+              }
+            });
+        } else if (silent === false) {
+          autoUpdater.quitAndInstall();
+          app.exit(0);
+        }
       }
-    });
+    );
   }
 };
