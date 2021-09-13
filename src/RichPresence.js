87ee980bc76ce2eae5464a77d38fa5f92417cb90
Faelayis <Faelayis@users.noreply.github.com>
2021-09-13 08:21:42 +0000

    Prettified Code!

--- src/RichPresence.js
+++ src/RichPresence.js
@@ -92,7 +92,9 @@ function formatBytes(freemem, totalmem, decimals = 0) {
   this.ramusage = `${parseFloat(
     (totalmem / k ** i - freemem / k ** i).toFixed(2 < 0 ? 0 : 2)
   )} `;
-  this.ram = `${parseFloat((totalmem / k ** i).toFixed(decimals < 0 ? 0 : decimals))} ${sizes[i]}`;
+  this.ram = `${parseFloat(
+    (totalmem / k ** i).toFixed(decimals < 0 ? 0 : decimals)
+  )} ${sizes[i]}`;
 }
 
 // Presence setActivity
