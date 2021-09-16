02157abffe0bb144d2d9483f4e2072f18d14223f
Faelayis <48393914+Faelayis@users.noreply.github.com>
2021-09-16 23:04:37 +0700

    add appimage

--- forge.config.js
+++ forge.config.js
@@ -48,6 +48,12 @@ module.exports = {
       name: "@electron-forge/maker-rpm",
       config: {},
     },
+    {
+      name: "electron-forge-maker-appimage",
+      platforms: [
+        "linux"
+      ]
+    }
   ],
   publishers: [
     {
